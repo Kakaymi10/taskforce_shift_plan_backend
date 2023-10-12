df7ec3e34adb757355eb7386416fb918b8f0e1d8
uwasejeannine <jeannineuwasee@gmail.com>
2023-10-12 22:58:24 +0200

    Committing changes before rebase

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -2,10 +2,13 @@ const express = require('express');
 const swaggerUi = require('swagger-ui-express')
 const authRoutes = require('./authRoutes');
 const swaggerFile = require('../swagger/swagger-output.json');
+const CompanyRoutes = require('./campanyRoutes');
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
+
+router.use('/companies', CompanyRoutes);
 
 router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
