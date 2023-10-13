c1eb383a249df15c9b6eb6f2b9329f2e5743f441
Kakaymi10 <m.moussa@alustdent.com>
2023-10-13 15:01:38 +0200

    Create CRUD operation for Department

--- backend/src/controllers/departmentControllers.js
+++ backend/src/controllers/departmentControllers.js
@@ -21,7 +21,7 @@ class DepartmentController {
 
       const department = await Department.create({ name, companyId });
       console.log('Department Created Successfully');
-      res.status(201).json({ success: 'Department created successfully', department });
+      res.status(201).json(department);
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Failed to create department' });
@@ -31,7 +31,7 @@ class DepartmentController {
   static async getAllDepartments(req, res) {
     try {
       const departments = await Department.findAll();
-      res.status(200).json({ success: 'Fetched all departments successfully', departments });
+      res.status(200).json(departments);
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Failed to fetch departments' });
@@ -41,7 +41,7 @@ class DepartmentController {
   static async deleteAllDepartments(req, res) {
     try {
       await Department.destroy({ where: {} });
-      res.status(204).json({ success: 'Deleted all departments' });
+    res.status(204).json({success: 'Deleted all departments'});
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Failed to delete all departments' });
@@ -53,7 +53,7 @@ class DepartmentController {
       const departmentId = req.params.id;
       const department = await Department.findByPk(departmentId);
       if (department) {
-        res.status(200).json({ success: 'Fetched department successfully', department });
+        res.status(200).json(department);
       } else {
         res.status(404).json({ error: 'Department not found' });
       }
@@ -83,7 +83,7 @@ class DepartmentController {
       const { name, companyId } = req.body;
       await department.update({ name, companyId });
       console.log('Updated');
-      res.status(200).json({ success: 'Updated department successfully', department });
+      res.status(200).json(department);
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Failed to update department' });
@@ -102,7 +102,7 @@ class DepartmentController {
       }
 
       await department.destroy();
-      res.status(204).json({ success: `Department with id: ${departmentId} deleted` });
+      res.status(204).json(`Department with id: ${departmentId} deleted`);
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Failed to delete department' });
--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const swaggerUi = require('swagger-ui-express')
 const authRoutes = require('./authRoutes');
 const swaggerFile = require('../swagger/swagger-output.json');
+const departmentRoutes = require('./departmentRoutes');
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
+router.use('/departments', departmentRoutes);
 
 router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
