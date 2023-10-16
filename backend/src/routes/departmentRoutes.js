898004046537ba9663c5b99688d574602d47dd03
Kakaymi10 <m.moussa@alustdent.com>
2023-10-16 20:19:20 +0200

    Add middlewares

--- backend/src/routes/departmentRoutes.js
+++ backend/src/routes/departmentRoutes.js
@@ -1,12 +1,14 @@
-// department-router.js
 const express = require('express');
 const router = express.Router();
-const DepartmentController = require('../controllers/departmentControllers'); // Import the DepartmentController
+const DepartmentController = require('../controllers/departmentControllers');
+const checkUserRole = require('../middlewares/checkUserRole'); 
 
-router.post('/', DepartmentController.createDepartment);
-router.get('/', DepartmentController.getAllDepartments);
+
+router.post('/', checkUserRole('Admin', 'Manager'), DepartmentController.createDepartment);
+router.get('/', checkUserRole('Admin', 'Manager'), DepartmentController.getAllDepartments);
 router.get('/:id', DepartmentController.getDepartmentById);
-router.put('/:id', DepartmentController.updateDepartmentById);
-router.delete('/:id', DepartmentController.deleteDepartmentById);
-router.delete('/', DepartmentController.deleteAllDepartments);
+router.put('/:id', checkUserRole('Admin', 'Manager'), DepartmentController.updateDepartmentById);
+router.delete('/:id', checkUserRole('Admin', 'Manager'), DepartmentController.deleteDepartmentById);
+router.delete('/', checkUserRole('Admin'), DepartmentController.deleteAllDepartments);
+
 module.exports = router;
