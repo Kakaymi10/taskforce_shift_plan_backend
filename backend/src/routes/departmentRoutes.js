8038f524ff06031b28e564f047ee891d2330a5f4
Moussa <109428479+Kakaymi10@users.noreply.github.com>
2023-10-17 07:51:30 +0200

    Add middlewares departmentRoutes.js

--- backend/src/routes/departmentRoutes.js
+++ backend/src/routes/departmentRoutes.js
@@ -1,14 +1,13 @@
 const express = require('express');
+const RoleController = require('../controllers/roleControllers');
+const checkUserRole = require('../middlewares/checkUserRole');
+
 const router = express.Router();
-const DepartmentController = require('../controllers/departmentControllers');
-const checkUserRole = require('../middlewares/checkUserRole'); 
 
-
-router.post('/', checkUserRole('Admin', 'Manager'), DepartmentController.createDepartment);
-router.get('/', checkUserRole('Admin', 'Manager'), DepartmentController.getAllDepartments);
-router.get('/:id', DepartmentController.getDepartmentById);
-router.put('/:id', checkUserRole('Admin', 'Manager'), DepartmentController.updateDepartmentById);
-router.delete('/:id', checkUserRole('Admin', 'Manager'), DepartmentController.deleteDepartmentById);
-router.delete('/', checkUserRole('Admin'), DepartmentController.deleteAllDepartments);
+router.get('/', RoleController.getAllRoles);
+router.get('/:id', RoleController.getRoleById);
+router.post('/', checkUserRole(['Admin', 'SuperAdmin']), RoleController.createRole);
+router.put('/:id', checkUserRole(['Admin', 'SuperAdmin']), RoleController.updateRole);
+router.delete('/:id', checkUserRole(['Admin', 'SuperAdmin']), RoleController.deleteRole);
 
 module.exports = router;
