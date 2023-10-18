e73ebb67c99cf4b12604eb27fbc6211b19e0c306
Kakaymi10 <m.moussa@alustdent.com>
2023-10-18 09:39:46 +0200

    Fixed bugs

--- backend/src/routes/departmentRoutes.js
+++ backend/src/routes/departmentRoutes.js
@@ -4,11 +4,11 @@ const DepartmentController = require('../controllers/departmentControllers');
 const checkUserRole = require('../middlewares/checkUserRole'); 
 
 
-router.post('/', checkUserRole('Admin', 'Manager'), DepartmentController.createDepartment);
-router.get('/', checkUserRole('Admin', 'Manager'), DepartmentController.getAllDepartments);
+router.post('/', checkUserRole(['Admin', 'Manager']), DepartmentController.createDepartment);
+router.get('/', checkUserRole(['Admin', 'Manager']), DepartmentController.getAllDepartments);
 router.get('/:id', DepartmentController.getDepartmentById);
-router.put('/:id', checkUserRole('Admin', 'Manager'), DepartmentController.updateDepartmentById);
-router.delete('/:id', checkUserRole('Admin', 'Manager'), DepartmentController.deleteDepartmentById);
-router.delete('/', checkUserRole('Admin'), DepartmentController.deleteAllDepartments);
+router.put('/:id', checkUserRole(['Admin', 'Manager']), DepartmentController.updateDepartmentById);
+router.delete('/:id', checkUserRole(['Admin', 'Manager']), DepartmentController.deleteDepartmentById);
+router.delete('/', checkUserRole(['Admin']), DepartmentController.deleteAllDepartments);
 
 module.exports = router;
