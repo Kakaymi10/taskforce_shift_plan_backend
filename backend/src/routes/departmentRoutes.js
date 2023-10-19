20832fe7fa28da2e89d5288b9cb9a761adb92f99
Kakaymi10 <m.moussa@alustdent.com>
2023-10-19 10:45:54 +0200

    Add absecnces CRUD.

--- backend/src/routes/departmentRoutes.js
+++ backend/src/routes/departmentRoutes.js
@@ -1,15 +1,13 @@
 const express = require('express');
-
+const RoleController = require('../controllers/roleControllers');
 const checkUserRole = require('../middlewares/checkUserRole');
-const DepartmentController = require('../controllers/departmentControllers');
+
 const router = express.Router();
 
-
-router.post('/',  DepartmentController.createDepartment);
-router.get('/',  DepartmentController.getAllDepartments);
-router.get('/:id', DepartmentController.getDepartmentById);
-router.put('/:id',  DepartmentController.updateDepartmentById);
-router.delete('/:id',  DepartmentController.deleteDepartmentById);
-router.delete('/',  DepartmentController.deleteAllDepartments);
+router.get('/', RoleController.getAllRoles);
+router.get('/:id', RoleController.getRoleById);
+router.post('/', checkUserRole(['Admin', 'SuperAdmin']), RoleController.createRole);
+router.put('/:id', checkUserRole(['Admin', 'SuperAdmin']), RoleController.updateRole);
+router.delete('/:id', checkUserRole(['Admin', 'SuperAdmin']), RoleController.deleteRole);
 
 module.exports = router;
